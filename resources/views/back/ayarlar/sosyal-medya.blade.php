fc5143cf23492bed83b7905db5716955f3a1c6d3
Emachintag <57269700+Emachintag@users.noreply.github.com>
2020-10-29 14:53:51 +0300

    e

---
+++ resources/views/back/ayarlar/sosyal-medya.blade.php
@@ -0,0 +1,160 @@
+@extends('back.layouts.app')
+@section('content')
+    <div class="app-content content">
+        <div class="content-wrapper">
+            <div class="content-header row">
+                <div class="content-header-left col-md-6 col-12 mb-2">
+                    <h3 class="content-header-title">Input Groups</h3>
+                    <div class="row breadcrumbs-top">
+                        <div class="breadcrumb-wrapper col-12">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="index.html">Home</a>
+                                </li>
+                                <li class="breadcrumb-item"><a href="#">Form Components</a>
+                                </li>
+                                <li class="breadcrumb-item active">Input Groups
+                                </li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+                <div class="content-header-right col-md-6 col-12">
+                    <div class="media width-250 float-right">
+                        <media-left class="media-middle">
+                            <div id="sp-bar-total-sales"></div>
+                        </media-left>
+                        <div class="media-body media-right text-right">
+                            <h3 class="m-0">$5,668</h3>
+                            <span class="text-muted">Sales</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="content-body">
+                <!-- input groups start -->
+
+                <!-- input groups end -->
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h4 class="card-title" id="row-separator-colored-controls">User Profile</h4>
+                                <a class="heading-elements-toggle"><i class="la la-ellipsis-h font-medium-3"></i></a>
+                                <div class="heading-elements">
+                                    <ul class="list-inline mb-0">
+                                        <li><a data-action="collapse"><i class="ft-minus"></i></a></li>
+                                        <li><a data-action="reload"><i class="ft-rotate-cw"></i></a></li>
+                                        <li><a data-action="expand"><i class="ft-maximize"></i></a></li>
+                                        <li><a data-action="close"><i class="ft-x"></i></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="card-content collapse show">
+                                <div class="card-body">
+                                    <form class="form form-horizontal row-separator">
+                                        <div class="form-body">
+                                            <h4 class="form-section"><i class="la la-eye"></i>Sosyal Medya</h4>
+                                            <div class="row">
+
+
+                                                <div class="col-md-4">
+                                                    <div class="row">
+                                                        <div class="card-body">
+                                                            <div class="card-block">
+                                                                <fieldset>
+                                                                    <div class="input-group">
+                                                                        <div class="input-group-prepend">
+                                                                            <span class="input-group-text" id="basic-addon3"><i class="la la-facebook"></i></span>
+                                                                        </div>
+                                                                        <input type="text" class="form-control" placeholder="Facebook" aria-describedby="basic-addon3">
+                                                                    </div>
+                                                                </fieldset>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-4">
+                                                    <div class="row">
+                                                        <div class="card-body">
+                                                            <div class="card-block">
+                                                                <fieldset>
+                                                                    <div class="input-group">
+                                                                        <div class="input-group-prepend">
+                                                                            <span class="input-group-text" id="basic-addon3"><i class="la la-twitter"></i></span>
+                                                                        </div>
+                                                                        <input type="text" class="form-control" placeholder="Twitter" aria-describedby="basic-addon3">
+                                                                    </div>
+                                                                </fieldset>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-4">
+                                                    <div class="row">
+                                                        <div class="card-body">
+                                                            <div class="card-block">
+                                                                <fieldset>
+                                                                    <div class="input-group">
+                                                                        <div class="input-group-prepend">
+                                                                            <span class="input-group-text" id="basic-addon3"><i class="la la-instagram"></i></span>
+                                                                        </div>
+                                                                        <input type="text" class="form-control" placeholder="İnstagram" aria-describedby="basic-addon3">
+                                                                    </div>
+                                                                </fieldset>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <div class="row">
+                                                        <div class="card-body">
+                                                            <div class="card-block">
+                                                                <fieldset>
+                                                                    <div class="input-group">
+                                                                        <div class="input-group-prepend">
+                                                                            <span class="input-group-text" id="basic-addon3"><i class="la la-youtube-play"></i></span>
+                                                                        </div>
+                                                                        <input type="text" class="form-control" placeholder="Youtube" aria-describedby="basic-addon3">
+                                                                    </div>
+                                                                </fieldset>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="card-body">
+                                                        <div class="card-block">
+                                                            <fieldset>
+                                                                <div class="input-group">
+                                                                    <div class="input-group-prepend">
+                                                                        <span class="input-group-text" id="basic-addon3"><i class="la la-linkedin"></i></span>
+                                                                    </div>
+                                                                    <input type="text" class="form-control" placeholder="Linkedin" aria-describedby="basic-addon3">
+                                                                </div>
+                                                            </fieldset>
+                                                        </div>
+                                                    </div>
+                                                </div>
+
+                                            </div>
+
+
+                                        </div>
+                                        <div class="form-actions right">
+                                            <button type="submit" class="btn btn-primary">
+                                                <i class="la la-check"></i> Güncelle
+                                            </button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+@endsection
--- resources/views/back/layouts/app.blade.php
+++ resources/views/back/layouts/app.blade.php
@@ -10,17 +10,17 @@
     <title>Dashboard sales - Modern Admin - Clean Bootstrap 4 Dashboard HTML Template + Bitcoin
         Dashboard
     </title>
-    <link rel="apple-touch-icon" href="{{asset('/public/app-assets/images/ico/apple-icon-120.png')}}">
-    <link rel="shortcut icon" type="image/x-icon" href="{{asset('/public/app-assets/images/ico/favicon.ico')}}">
+    <link rel="apple-touch-icon" href="{{asset('/public/back/app-assets/images/ico/apple-icon-120.png')}}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{asset('/public/back/app-assets/images/ico/favicon.ico')}}">
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Quicksand:300,400,500,700"
           rel="stylesheet">
     <link href="https://maxcdn.icons8.com/fonts/line-awesome/1.1/css/line-awesome.min.css"
           rel="stylesheet">
     <!-- BEGIN VENDOR CSS-->
-    <link rel="stylesheet" type="text/css" href="{{asset('/public/app-assets/css/vendors.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('/public/back/app-assets/css/vendors.css')}}">
     <!-- END VENDOR CSS-->
     <!-- BEGIN MODERN CSS-->
-    <link rel="stylesheet" type="text/css" href="{{asset('/public/app-assets/css/app.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('/public/back/app-assets/css/app.css')}}">
     <!-- END MODERN CSS-->
     <!-- BEGIN Page Level CSS-->
     <link rel="stylesheet" type="text/css" href="{{asset('/public/back/app-assets/css/core/menu/menu-types/horizontal-menu.css')}}">
@@ -31,7 +31,7 @@
     <link rel="stylesheet" type="text/css" href="{{asset('/public/back/app-assets/css/core/colors/palette-gradient.css')}}">
     <!-- END Page Level CSS-->
     <!-- BEGIN Custom CSS-->
-    <link rel="stylesheet" type="text/css" href="{{asset('/public/assets/css/style.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('/public/back/assets/css/style.css')}}">
     <!-- END Custom CSS-->
 </head>
 <body class="horizontal-layout horizontal-menu 2-columns   menu-expanded" data-open="hover"
@@ -521,7 +521,7 @@
     </p>
 </footer>
 <!-- BEGIN VENDOR JS-->
-<script src="{{asset('/public/app-assets/vendors/js/vendors.min.js')}}" type="text/javascript"></script>
+<script src="{{asset('/public/back/app-assets/vendors/js/vendors.min.js')}}" type="text/javascript"></script>
 <!-- BEGIN VENDOR JS-->
 <!-- BEGIN PAGE VENDOR JS-->
 <script type="text/javascript" src="{{asset('/public/back/app-assets/vendors/js/ui/jquery.sticky.js')}}"></script>
