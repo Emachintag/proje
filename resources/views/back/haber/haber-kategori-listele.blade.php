bd0d0baf130eca29f24d0647e9762d253044f09c
Emachintag <57269700+Emachintag@users.noreply.github.com>
2020-10-29 21:21:08 +0300

    haber hazir

---
+++ resources/views/back/haber/haber-kategori-listele.blade.php
@@ -0,0 +1,128 @@
+@section('css')
+    <link rel="stylesheet" type="text/css" href="{{asset('/public/back/app-assets/vendors/css/tables/datatable/datatables.min.css')}}">
+@endsection
+@extends('back.layouts.app')
+@section('content')
+    <div class="app-content content">
+        <div class="content-wrapper">
+            <div class="content-body">
+
+                <section id="stats-icon-subtitle-bg">
+
+                    <div class="row">
+                        <div class="col-xl-6 col-md-12">
+                            <div class="card">
+                                <div class="card-content">
+                                    <div class="media align-items-stretch bg-warning text-white rounded">
+                                        <div class="bg-warning bg-darken-2 p-2 media-middle">
+                                            <i class="icon-speech font-large-2 text-white"></i>
+                                        </div>
+                                        <div class="media-body p-2">
+                                            <h4 class="text-white">Haber Kategori Sayısı</h4>
+                                            <span>Kaydedilen Haber Kategori Sayısı</span>
+                                        </div>
+                                        <div class="media-right p-2 media-middle">
+                                            <h1 class="text-white">84,695</h1>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-6 col-md-12">
+                            <div class="card overflow-hidden">
+                                <div style="cursor: pointer" onclick="location.href='{{route('blog_kategori_ekle')}}'" class="card-content">
+                                    <div  class="media align-items-stretch bg-info text-white rounded">
+                                        <div class="bg-info bg-darken-2 p-2 media-middle">
+                                            <i class="icon-pencil font-large-2 text-white"></i>
+                                        </div>
+                                        <div class="media-body p-2">
+                                            <h4 class="text-white">Yeni Haber Kategori Ekle</h4>
+                                            <span>Yeni Haber Kategori Eklemek İçin Tıklayınız</span>
+                                        </div>
+                                        <div class="media-right p-2 media-middle">
+                                            <h1 class="text-white">18,000</h1>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </section>
+                <!-- Zero configuration table -->
+                <section id="configuration">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="card">
+                                <div class="card-header">
+                                    <h4 class="card-title">Haber Listeleme</h4>
+                                    <a class="heading-elements-toggle"><i class="la la-ellipsis-v font-medium-3"></i></a>
+                                    <div class="heading-elements">
+                                        <ul class="list-inline mb-0">
+                                            <li><a data-action="collapse"><i class="ft-minus"></i></a></li>
+                                            <li><a data-action="reload"><i class="ft-rotate-cw"></i></a></li>
+                                            <li><a data-action="expand"><i class="ft-maximize"></i></a></li>
+                                            <li><a data-action="close"><i class="ft-x"></i></a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                                <div class="card-content collapse show">
+                                    <div class="card-body card-dashboard">
+
+                                        <table class="table table-striped table-bordered zero-configuration">
+                                            <thead>
+                                            <tr>
+                                                <th>Haber Kategori Başlık</th>
+                                                <th>Haber Kategori Sırası</th>
+                                                <th>İşlem</th>
+
+                                            </tr>
+                                            </thead>
+                                            <tbody>
+                                            <tr>
+                                                <td>Donna Snider</td>
+                                                <td>Customer Support</td>
+                                                <td>
+                                                    <a href="?haber=&sil&token={{ csrf_token() }}" class="btn btn-danger btn-min-width btn-glow">
+                                                        <i class="la la-trash"></i>
+                                                        <span>
+                                                Sil
+                                            </span>
+                                                    </a>
+                                                    <a href="/haber-duzenle/" class="btn btn-info btn-min-width btn-glow">
+                                                        <i class="la la-edit"></i>
+                                                        <span>
+                                                Düzenle
+                                            </span>
+                                                    </a>
+                                                </td>
+
+                                            </tr>
+                                            </tbody>
+                                            <tfoot>
+                                            <tr>
+                                                <th>Haber Kategori Başlık</th>
+                                                <th>Haber Kategori Sırası</th>
+                                                <th>İşlem</th>
+
+                                            </tr>
+                                            </tfoot>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <!--/ Zero configuration table -->
+
+            </div>
+        </div>
+    </div>
+
+
+@endsection
+@section('js')
+    <script src="{{asset('/public/back/app-assets/vendors/js/tables/datatable/datatables.min.js')}}" type="text/javascript"></script>
+    <script src="{{asset('/public/back/app-assets/js/scripts/tables/datatables/datatable-basic.js')}}" type="text/javascript"></script>
+@endsection
