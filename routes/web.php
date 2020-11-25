4e14f8961af0f23f9b92fb5ebba41e72671da180
S. Mert ÖZTÜRK <40407984+sameter123@users.noreply.github.com>
2020-11-25 15:21:36 +0300

    v3

--- routes/web.php
+++ routes/web.php
@@ -155,18 +155,18 @@ Route::prefix('panel')->group(function () {
 
     // Üye İşlemleri Bitiş
 
-
-    foreach (Modules::where('status', '1')->get() as $u) {
-        $moduleLink = $u->moduleLink;
-        $moduleController = $u->moduleController;
-        $space = "App\Http\Controllers\\";
-        $controller = $space.$moduleController;
-        $moduleSlug = $u->moduleSlug;
-        Route::get($moduleLink, $controller.'@listele')->name($moduleSlug);
-        Route::get($moduleLink, $controller.'@ekle')->name($moduleSlug.'_ekle');
-        Route::post($moduleLink, $controller.'@ekle_post')->name($moduleSlug.'_ekle_post');
-        Route::get($moduleLink, $controller.'@duzenle')->name($moduleSlug.'_duzenle');
-        Route::post($moduleLink, $controller.'@duzenle_post')->name($moduleSlug.'_duzenle_post');
-    }
-
+    /*
+        foreach (Modules::where('status', '1')->get() as $u) {
+            $moduleLink = $u->moduleLink;
+            $moduleController = $u->moduleController;
+            $space = "App\Http\Controllers\\";
+            $controller = $space.$moduleController;
+            $moduleSlug = $u->moduleSlug;
+            Route::get($moduleLink, $controller.'@listele')->name($moduleSlug);
+            Route::get($moduleLink, $controller.'@ekle')->name($moduleSlug.'_ekle');
+            Route::post($moduleLink, $controller.'@ekle_post')->name($moduleSlug.'_ekle_post');
+            Route::get($moduleLink, $controller.'@duzenle')->name($moduleSlug.'_duzenle');
+            Route::post($moduleLink, $controller.'@duzenle_post')->name($moduleSlug.'_duzenle_post');
+        }
+    */
 });
